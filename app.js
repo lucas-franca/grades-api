895ef4f7d748f3c1208d5f91772715afc64a00bb
primobec <lucas.franca.94@hotmail.com>
2020-09-03 22:48:51 -0300

    Ajustes no cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: `${ADDRESS}`,
+    origin: PORT === '' ? ADDRESS : `${ADDRESS}:${PORT}`,
   })
 );
 
